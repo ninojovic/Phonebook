30fe82399124c288f9820a05c20e025de659804d
ninojovic <ninojovic@gmail.com>
2018-06-12 17:41:20 +0200

    connected postgresql

--- server/index.js
+++ server/index.js
@@ -2,6 +2,22 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString:process.env.DATABASE_URL,
+  ssl: true,
+})
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 const PORT = process.env.PORT || 5000;
 
